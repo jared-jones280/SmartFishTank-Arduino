f4aec6ac28c0bcf479324fa9a05943cbf3423dac
Charlemagne Wong <frian13@hotmail.com>
2020-04-21 14:55:05 -0500

    Updates

--- pi/fish-tank.py
+++ pi/fish-tank.py
@@ -1,15 +1,24 @@
 import time
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
+import serial
 
-kit = MotorKit()
 
-kit.stepper1.release()
 
-time.sleep(5)
+def serial_test():
+    ser = serial.Serial('/dev/ttyACM0')
 
-for i in range(20):
-    kit.stepper1.onestep(style=stepper.SINGLE)
-    time.sleep(0.5)
-time.sleep(0.5)
-kit.stepper1.release()
+# kit = MotorKit()
+
+# kit.stepper1.release()
+
+# time.sleep(5)
+
+# for i in range(20):
+#     kit.stepper1.onestep(style=stepper.SINGLE)
+#     time.sleep(0.5)
+# time.sleep(0.5)
+# kit.stepper1.release()
+
+if __name__ == '__main__':
+    main()

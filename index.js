e95a71c09ec7c136934dd4138df90d48e3ca7963
Jared Jones <jared.jones280@tamu.edu>
2020-04-22 17:50:23 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+while(!false){
+    setTimeout(function(){
+
+    //put http request here and store data in following var
+         var temp1;
+         var temp2;
+         var avtTemp;
+         var pottemp;
+         var ph;
+         var clarity;
+
+         document.getElementById("temp").innerText = "Temp: ";
+         document.getElementById("pottemp").innerText = "Set Temp: ";
+         document.getElementById("ph").innerText = "PH: ";
+         document.getElementById("clarity").innerText = "Clarity: ";
+
+
+
+
+
+    },2000);
+
+
+    
+}

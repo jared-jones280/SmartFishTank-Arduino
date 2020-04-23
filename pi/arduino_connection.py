08922edd338ae77d1d6b3ba4fcf73de086a9a22d
Charlemagne Wong <frian13@hotmail.com>
2020-04-23 00:55:02 -0500

    Update code - mostly working

--- pi/arduino_connection.py
+++ pi/arduino_connection.py
@@ -6,16 +6,20 @@ class ArduinoConnection:
     def __init__(self, serial):
         self.serial = serial
 
-    def get(self, command):
+    def get(self, command, nice=True, timeout=0.5, wait=0.5):
         command = command.rstrip() + ' '
         self.serial.write(command.encode())
 
-        while self.serial.in_waiting == 0:
+        start = time.time()
+        
+        while self.serial.in_waiting == 0 and time.time() - start < timeout:
             time.sleep(0.05)
         
-        return self.serial.read(self.serial.in_waiting)
+        time.sleep(wait)
+        
+        return clean(self.serial.read(self.serial.in_waiting).decode(), nice=nice)
 
-    def read(self, buffer_size=-1):
+    def read(self, buffer_size=-1, nice=True):
         in_waiting = self.serial.in_waiting
         
         if buffer_size == -1:
@@ -24,4 +28,16 @@ class ArduinoConnection:
         if in_waiting == 0:
             return ('', 0)
         else:
-            return (self.serial.read(buffer_size), min(buffer_size, in_waiting))
+            return (clean(self.serial.read(buffer_size).decode(), nice=nice), min(buffer_size, in_waiting))
+
+    def refresh_out(self):
+        self.read(nice=False)
+
+def clean(str, nice=True, decode=True):
+    if str is bytes:
+        str = str.decode()
+    
+    while nice and len(str) != 0 and (str[-1] == '\n' or str[-1] == '\r'):
+        str = str[:-1]
+    
+    return str
--- pi/driver.py
+++ pi/driver.py
@@ -1,8 +1,45 @@
 from arduino_connection import ArduinoConnection
 import serial
+import fish_tank
+import socket
+import json
+import pickle
+import time
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.050)
-serial_connection = ArduinoConnection(ser)
+ser = fish_tank.connect_arduino()
+if ser is None:
+    print('Unable to find serial connection')
+    exit(1)
+arduino = ArduinoConnection(ser)
+
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('192.168.1.53', 8001))
+except socket.error:
+    print('Unable to connect to server')
+
+handshake_data = {'device' : 'smart-fish-tank',
+        'accept' : True
+        }
+
+s.send(json.dumps(handshake_data).encode())
+
+time.sleep(2)
+
+arduino.refresh_out()
+
+tankData = {
+                'temperature-0' : float(arduino.get('temperature-sensor 0')),
+                'temperature-1' : float(arduino.get('temperature-sensor 1')),
+                'ph'            : float(arduino.get('ph')),
+                'turbidity'     : float(arduino.get('turbidity')),
+                'test'          : 0.32,
+                'alarm'         : False
+            }
+s.send(json.dumps(tankData).encode())
+
+# fish_tank.main()
 
 # if __name__ in '__main__':
 #     main();

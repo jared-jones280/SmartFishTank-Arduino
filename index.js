815772e899dc7731c36a516df84bb3ab5a3f689b
Jared Jones <jared.jones280@tamu.edu>
2020-04-26 13:52:22 -0500

    working sensor values

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-while(!false){
-    setTimeout(function(){
-
+function refreshValues(){
+    var request = new XMLHttpRequest();
+    request.open('GET','http://pi.cmasterx.com:8000/api');
+    request.onload=function(){
+        if(request.status==200){
     //put http request here and store data in following var
-         var temp1;
-         var temp2;
-         var avtTemp;
-         var pottemp;
-         var ph;
-         var clarity;
+             var sensorData = JSON.parse(this.response);
+             var temp0=sensorData["temperature-0"];
+             var temp1=sensorData["temperature-1"];
+             var avTemp=(temp0+temp1)/2;
+             var pottemp=sensorData["potentiometer-0"];
+             var ph=sensorData.ph;
+             var clarity=sensorData.turbidity;
+             var alarm=sensorData.alarm;
 
-         document.getElementById("temp").innerText = "Temp: ";
-         document.getElementById("pottemp").innerText = "Set Temp: ";
-         document.getElementById("ph").innerText = "PH: ";
-         document.getElementById("clarity").innerText = "Clarity: ";
+            document.getElementById("temp").innerText = "Temperature Conditions: "+avTemp;
+            document.getElementById("pottemp").innerText = "Set Temperature: "+pottemp;
+            document.getElementById("ph").innerText = "PH: "+ph;
+            document.getElementById("clarity").innerText = "Clarity: "+clarity;
+            document.getElementById("alarm").innerText= "Alarm: "+alarm;
+        }
+        else{
+            console.log("error with http req");
+        }
+    }
+    request.send();
 
 
-
-
-
-    },2000);
-
-
-    
-}
+}   
